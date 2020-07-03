f659a3c1ceb90c666f6961fed939ded7c45c9bc4
Ryan Mehri <ryan.mehri1@gmail.com>
2020-07-02 22:24:10 -0600

    Add watch list algebra

--- core/src/main/scala/housingfinder/algebras/kijiji.scala
+++ core/src/main/scala/housingfinder/algebras/kijiji.scala
@@ -4,6 +4,6 @@ import housingfinder.domain.kijiji.Listing
 
 trait Kijiji[F[_]] {
   // TODO: some way to filter out listings by desired properties
-  def findListings: F[List[Listing]]
+  def getListings: F[List[Listing]]
   def updateListings: F[Unit]
 }
